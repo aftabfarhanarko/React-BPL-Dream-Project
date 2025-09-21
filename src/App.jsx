22d0dda236bd37964fb3f5c625328823b00ec1a9
Aftab Farhan <aftabfarhan324@gmail.com>
2025-09-22 02:03:08 +0600

    complet navber desing

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
 import "./App.css";
+import navLogo from './assets/logo.png'
+import coins from './assets/Currency.png'
 
 function App() {
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar max-w-[1150px] mx-auto">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className=" btn-ghost text-xl">
+            <img className="w-[70px] h-[60px]" src={navLogo}></img>
+          </a>
         </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>{" "}
-            </svg>
-          </button>
+        <div className="flex items-center">
+           <span className="mr-1">6000000000</span>
+           <span className="mr-1">Coin</span>
+           <img src={coins}></img>
         </div>
       </div>
     </>
