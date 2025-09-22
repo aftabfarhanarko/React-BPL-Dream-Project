30209f6cf8a278c2a2c5e6c0e21595ecf8bb3481
Aftab Farhan <aftabfarhan324@gmail.com>
2025-09-23 00:36:34 +0600

    fix vite base path for netlify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: '/', // এই লাইনটা খুব গুরুত্বপূর্ণ Netlify deploy এর জন্য
 })
